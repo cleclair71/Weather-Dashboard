fe17003648fc0f0ffaa78325a51785835ae012b6
cass <76407605+cleclair71@users.noreply.github.com>
2023-02-02 23:37:13 -0500

    tried to fix the js

--- Assets/script.js
+++ Assets/script.js
@@ -3,147 +3,113 @@ const APIKey = "57deb76a90251740c9398ca04eb2710e";
 const form = document.querySelector("#input");
 const searchInput = document.querySelector("#search-input");
 const btn = document.querySelector(".btn");
+
 // Get current date and time
 const currentDate = document.querySelector("#current-date");
 currentDate.textContent = dayjs().format("MMM/DD, YYYY");
 
-// Forecast
-const date1 = document.querySelector("#date1");
-const date2 = document.querySelector("#date2");
-const date3 = document.querySelector("#date3");
-const date4 = document.querySelector("#date4");
-const date5 = document.querySelector("#date5");
-const icon1 = document.querySelector("#img1");
-const icon2 = document.querySelector("#img2");
-const icon3 = document.querySelector("#img3");
-const icon4 = document.querySelector("#img4");
-const icon5 = document.querySelector("#img5");
-const weather1 = document.querySelector("#day1 .weather");
-const weather2 = document.querySelector("#day2 .weather");
-const weather3 = document.querySelector("#day3 .weather");
-const weather4 = document.querySelector("#day4 .weather");
-const weather5 = document.querySelector("#day5 .weather");
-const temp1 = document.querySelector("#day1temp");
-const temp2 = document.querySelector("#day2temp");
-const temp3 = document.querySelector("#day3temp");
-const temp4 = document.querySelector("#day4temp");
-const temp5 = document.querySelector("#day5temp");
-const hum1 = document.querySelector("#day1hum");
-const hum2 = document.querySelector("#day2hum");
-const hum3 = document.querySelector("#day3hum");
-const hum4 = document.querySelector("#day4hum");
-const hum5 = document.querySelector("#day5hum");
-const speed1 = document.querySelector("#day1speed");
-const speed2 = document.querySelector("#day2speed");
-const speed3 = document.querySelector("#day3speed");
-const speed4 = document.querySelector("#day4speed");
-const speed5 = document.querySelector("#day5speed");
-
 btn.addEventListener("click", async (event) => {
-    event.preventDefault();
-    const city = searchInput.value;
-    const weatherData = await getWeatherData(city, APIKey);
-    const forecastData = await getForecastData(city, APIKey);
-    displayCurrentWeatherData(weatherData);
+  event.preventDefault();
+  const city = searchInput.value;
+  const weatherData = await getWeatherData(city, APIKey);
+  const forecastData = await getForecastData(city, APIKey);
+  displayCurrentWeatherData(weatherData);
 });
 
 async function getWeatherData(city, APIKey) {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
-    );
-    return await response.json();
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
+  );
+  return await response.json();
+}
+
+function displayCurrentWeatherData(currentWeatherData) {
+  const displayCity = document.querySelector("#display-city");
+  if (displayCity) {
+    displayCity.textContent = data.name;
   }
-  
-  function displayCurrentWeatherData(data) {
-    const displayCity = document.querySelector("#display-city");
-    if (displayCity) {
-      displayCity.textContent = data.name;
-    }
-    const currentTime = document.querySelector("#current-time");
-    currentTime.textContent = dayjs().format("h:mm:ss a");
-    const temp = document.querySelector("#temp");
-    temp.textContent = `Temperature: ${data.main.temp} °F`;
-    const humidity = document.querySelector("#humidity");
-    humidity.textContent = `Humidity: ${data.main.humidity}%`;
-    const windSpeed = document.querySelector("#wind-speed");
-    windSpeed.textContent = `Wind Speed: ${data.wind.speed} mph`;
-    const main = document.querySelector("#main");
+  const currentTime = document.querySelector("#current-time");
+  currentTime.textContent = dayjs().format("h:mm:ss a");
+  const temp = document.querySelector("#temp");
+  temp.textContent = `Temperature: ${data.main.temp} °F`;
+  const humidity = document.querySelector("#humidity");
+  humidity.textContent = `Humidity: ${data.main.humidity}%`;
+  const windSpeed = document.querySelector("#wind-speed");
+  windSpeed.textContent = `Wind Speed: ${data.wind.speed} mph`;
+  const main = document.querySelector("#main");
     main.textContent = data.weather[0].main;
     const description = document.querySelector("#description");
     description.textContent = data.weather[0].description;
     const image = document.querySelector("#image");
     image.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
   }
-  
 
- // Display recent searches
- let recentSearches = JSON.parse(localStorage.getItem("recentSearches")) || [];
- const recentList = document.querySelector("#recent");
-
- // Add city to recent searches and limit to 4
- const city = searchInput.value;
- if (!recentSearches.includes(city)) {
-   recentSearches.unshift(city);
-   if (recentSearches.length > 4) {
-     recentSearches.pop();
-   }
- }
-
- localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
-
- // Clear recent list
- recentList.innerHTML = "";
-
- // Render recent searches
- for (let i = 0; i < recentSearches.length; i++) {
-   let li = document.createElement("li");
-   li.textContent = recentSearches[i];
-   recentList.appendChild(li);
- }
-
- var forecast = document.querySelector(".forecast");
-
- async function cityGeo (city, APIKey) {
-     const geoURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIKey}`;
-     const response = await fetch(geoURL);
-     const geoData = await response.json();
-     const lat = geoData[0].lat;
-     const lon = geoData[0].lon;
-     return {lat, lon};
- }
- 
- 
- async function getForecastData(city, APIKey) {
-     const coords = await cityGeo(city, APIKey);
-     const lat = coords.lat;
-     const lon = coords.lon;
- 
-     const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`; 
-     const response = await fetch(forecastURL);
-     const data = await response.json();
- 
-
-let forecastData = data.list.filter((item, index) => {
-         return index % 8 === 0;
-     });
-     
-for (let i = 1; i <= 5; i++) {
-    let forecastDate = dayjs.unix(forecastData[i - 1].dt).format('MMMM D, YYYY');
-    const dateEl = document.querySelector(`#date${i}`);
-    if (!dateEl) {
-        console.error(`Element with id date not found.`);
-        continue;
-    }
-    dateEl.textContent = forecastDate;
-  
-    const iconUrl = `http://openweathermap.org/img/wn/${forecastData[i - 1].weather[0].icon}@2x.png`;
-    document.querySelector(`#img${i}`).setAttribute("src", iconUrl);
-  
-    document.querySelector(`#day${i}`).textContent = forecastData[i - 1].weather[0].description;
-    
-    const dayTemp = (forecastData[i - 1].main.temp - 273.15).toFixed(0);
-    document.querySelector(`#day${i}temp`).textContent = `Temperature: ${dayTemp}°C`;
-    document.querySelector(`#day${i}hum`).textContent = `Humidity: ${forecastData[i - 1].main.humidity}%`;
-    document.querySelector(`#day${i}speed`).textContent = `Wind Speed: ${forecastData[i - 1].wind.speed} mph`;
+// Display recent searches
+let recentSearches = [];
+try {
+  recentSearches = JSON.parse(localStorage.getItem("recentSearches")) || [];
+} catch (e) {
+  console.error("Error parsing recentSearches from local storage", e);
 }
- }
+const recentList = document.querySelector("#recent");
+
+// Add city to recent searches and limit to 5
+const city = searchInput.value;
+if (!recentSearches.includes(city)) {
+  recentSearches.unshift(city);
+  if (recentSearches.length > 5) {
+    recentSearches.pop();
+  }
+}
+
+// Clear recent list
+recentList.innerHTML = "";
+
+
+// Render recent searches
+for (let i = 0; i < recentSearches.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = recentSearches[i];
+    recentList.appendChild(li);
+    }
+    localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
+    displayCurrentWeatherData();
+    
+    var forecast = document.querySelector(".forecast");
+    
+    async function cityGeo(city, APIKey) {
+    const geoURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIKey}`;
+    const response = await fetch(geoURL);
+    const geoData = await response.json();
+    const lat = geoData[0].lat;
+    const lon = geoData[0].lon;
+    return { lat, lon };
+    }
+    
+    async function getForecastData(city, APIKey) {
+    const coords = await cityGeo(city, APIKey);
+    const lat = coords.lat;
+    const lon = coords.lon;
+    
+    const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+    const response = await fetch(forecastURL);
+    const data = await response.json();
+    const forecastData = data.list.filter(
+    (reading) => reading.dt_txt.includes("12:00:00")
+    );
+    
+    forecastData.forEach((reading, index) => {
+    const date = new Date(reading.dt_txt);
+    document.getElementById(`date${index + 1}`).innerHTML = date.toDateString();
+    document.getElementById(`img${index + 1}`).src = `http://openweathermap.org/img/wn/${reading.weather[0].icon}@2x.png`;
+    document.getElementById(`daytemp${index + 1}`).innerHTML = `Temperature: ${reading.main.temp}°F`;
+    document.getElementById(`dayhum${index + 1}`).innerHTML = `Humidity: ${reading.main.humidity}%`;
+    document.getElementById(`dayspeed${index + 1}`).innerHTML = `Wind-speed: ${reading.wind.speed}mph`;
+    });
+    }
+    
+    getForecastData(city, APIKey);
+    
+    
+    
+    
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 <!-- PROJECT LOGO -->
 
-  <h3 align="center">Work Day Schedule</h3>
+  <h3 align="center">Weather Dashboard</h3>
 
 <!-- TABLE OF CONTENTS -->
 <details>
@@ -28,9 +28,8 @@
 <!-- ABOUT THE PROJECT -->
 ## About The Project
 
-![screen1](https://user-images.githubusercontent.com/76407605/214712756-469e0aea-e7b2-469d-af5c-0c4242c202e2.JPG)
-
-This application assists busy employees in organizing and managing their time by providing a convenient way to track important events. The project meets acceptance criteria and follows the design specifications outlined in the provided mock-up.
+![Capture](https://user-images.githubusercontent.com/76407605/216513450-9c848f98-22f9-45b1-a0f4-047f51b4f6e4.JPG)
+![Capture2](https://user-images.githubusercontent.com/76407605/216513452-9745841a-c30d-4522-a7cd-fb98b8b572b9.JPG)
 
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
@@ -40,32 +39,20 @@ This application assists busy employees in organizing and managing their time by
 <!-- GETTING STARTED -->
 ## Getting Started
 
-This planner features a calendar that displays the current day at the top, and uses a date library to handle date and time functionality. Scrolling down shows time blocks for standard business hours, which are color-coded to indicate past, present, and future events. Users can click on a time block to add an event, and the save button stores the event in local storage for persistence upon page refresh. Users can also edit entries and use the clear button to clear the contents and local storage.
+This weather dashboard allows users to retrieve up-to-date weather information for cities by searching for a city and adding it to the search history. It presents current and future weather conditions for the selected city, including the city name, date, weather icon, temperature, humidity, and wind speed.
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-
-
-<!-- USAGE EXAMPLES -->
-## Usage
-![screen1](https://user-images.githubusercontent.com/76407605/214712756-469e0aea-e7b2-469d-af5c-0c4242c202e2.JPG)
-![screen2](https://user-images.githubusercontent.com/76407605/214712757-54441117-7c50-492d-8a4f-045cb9c4264e.JPG)
-![screen3](https://user-images.githubusercontent.com/76407605/214712758-e2d516e5-0ad0-4013-8e66-09bced0a858b.JPG)
-![screen4](https://user-images.githubusercontent.com/76407605/214712759-121fc383-5b2f-4e53-a509-13e4e0ecb29f.JPG)
-![screen5](https://user-images.githubusercontent.com/76407605/214712761-c7b2ceea-8f9b-4cac-8537-5603ef8a72d3.JPG)
-<p align="right">(<a href="#readme-top">back to top</a>)</p>
-
 
 
 <!-- ROADMAP -->
 ## Roadmap
 
-- [x] Current day is displayed at the top of the calendar
-- [x] Presented with timeblocks for standard business hours
-- [x] Each timeblock is color coded to indicate whether it is in the past, present, or future
-- [x] Can enter an event into the timeblock
-- [x] Text for that event is saved in local storage
-- [x] The saved event remains when the page is refreshed
+- [ ]  Weather dashboard with form inputs to search for a city
+- [ ] Upon search, current and future weather conditions are displayed for the searched city, along with its name, date, weather icon, temperature, humidity, and wind speed
+- [x]  5-day forecast displayed for future weather conditions
+- [x] Search history keeps track of previous searches and allows easy access to their conditions by clicking on the city name
+- [x] Weather conditions update with current information when the city is clicked on in search history
+- [x] Search history and conditions persist even after the page is refreshed.
 
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
@@ -85,8 +72,8 @@ Distributed under the MIT License. See `LICENSE.txt` for more information.
 
 Cassie LeClair - cassieleclair71@gmail.com
 
-* Deployed Website: [https://cleclair71.github.io/05-workday-calendar/](https://cleclair71.github.io/05-workday-calendar/)
-* Repository Link: [https://github.com/cleclair71/05-workday-calendar](https://github.com/cleclair71/05-workday-calendar)
+* Deployed Website: [https://cleclair71.github.io/Weather-Dashboard/](https://cleclair71.github.io/Weather-Dashboard/)
+* Repository Link: [https://github.com/cleclair71/Weather-Dashboard](https://github.com/cleclair71/Weather-Dashboard)
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
 
@@ -96,10 +83,6 @@ Cassie LeClair - cassieleclair71@gmail.com
 ## Acknowledgments
 
 * [README Template](https://github.com/othneildrew/Best-README-Template)
-* [Spacing](https://getbootstrap.com/docs/4.0/utilities/spacing/)
-* [Format](https://day.js.org/docs/en/display/format)
-* [Bootstrap layout](https://getbootstrap.com/docs/5.0/layout/utilities/)
-* [jQuery API](https://api.jquery.com/)
 * [Document.getElementById()](https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById)
 * [An Interactive Guide to Keyframe Animations](https://www.joshwcomeau.com/animation/keyframe-animations/)
 * [return](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/return)
@@ -107,16 +90,3 @@ Cassie LeClair - cassieleclair71@gmail.com
 * [Animated Search Bar Using Only HTML and CSS](https://foolishdeveloper.com/animated-search-bar-using-only-html-and-css/)
 * [Bootstrap 4 weather widget with time and date](https://bbbootstrap.com/snippets/weather-widget-time-and-date-51644824)
 
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
-
-Use the 5 Day Weather Forecast Links to an external site.to retrieve weather data for cities. The base URL should look like the following: https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate.
-
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
