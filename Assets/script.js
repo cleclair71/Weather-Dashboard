0c2501f6efa2080bd861d672b9b55f7de2db841a
cleclair71 <cassieleclair71@gmail.com>
2023-05-20 13:10:30 -0400

    merging branches

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,3 @@
-
 var owmAPI = "57deb76a90251740c9398ca04eb2710e";
 var currentCity = "";
 var lastCity = "";
@@ -12,34 +11,34 @@ var handleErrors = (response) => {
 var getCurrentConditions = (event) => {
   // Obtain city name from the search box
   let city = $('#search-city').val();
-  currentCity= $('#search-city').val();
+  currentCity = $('#search-city').val();
   // Set the queryURL to fetch from API using weather search - added units=imperial to fix
   let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + owmAPI;
   fetch(queryURL)
-  .then(handleErrors)
-  .then((response) => {
+    .then(handleErrors)
+    .then((response) => {
       return response.json();
-  })
+    })
 
-  .then((response) => {
-        // Save city to local storage
-        saveCity(city);
-        $('#search-error').text("");
-        // Create icon for the current weather using Open Weather Maps
-        let currentWeatherIcon="https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
-        // Offset UTC timezone - using moment.js
-        let currentTimeUTC = response.dt;
-        let currentTimeZoneOffset = response.timezone;
-        let currentTimeZoneOffsetHours = currentTimeZoneOffset / 60 / 60;
-        let currentMoment = moment.unix(currentTimeUTC).utc().utcOffset(currentTimeZoneOffsetHours);
+    .then((response) => {
+      // Save city to local storage
+      saveCity(city);
+      $('#search-error').text("");
+      // Create icon for the current weather using Open Weather Maps
+      let currentWeatherIcon = "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+      // Offset UTC timezone - using moment.js
+      let currentTimeUTC = response.dt;
+      let currentTimeZoneOffset = response.timezone;
+      let currentTimeZoneOffsetHours = currentTimeZoneOffset / 60 / 60;
+      let currentMoment = moment.unix(currentTimeUTC).utc().utcOffset(currentTimeZoneOffsetHours);
 
-  renderCities();
+      renderCities();
 
-  getFiveDayForecast(event);
+      getFiveDayForecast(event);
 
-  $('#header-text').text(response.name);
+      $('#header-text').text(response.name);
 
-  let currentWeatherHTML = `
+      let currentWeatherHTML = `
   <span style="font-size: 2em;color: #2713347b;">${currentMoment.format("MM/DD/YY")}</span>
   <h3 style="font-family: 'Montserrat', sans-serif; text-transform: uppercase; font-size: 3em; color: #2713347b; font-weight: 600;">${response.name}
   
@@ -54,29 +53,29 @@ var getCurrentConditions = (event) => {
   <li id="uvIndex">UV Index:</li>
 </ul>`;
 
-  $('#current-weather').html(currentWeatherHTML);
+      $('#current-weather').html(currentWeatherHTML);
 
-  let latitude = response.coord.lat;
-  let longitude = response.coord.lon;
-  let uvQueryURL = "api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&APPID=" + owmAPI;
+      let latitude = response.coord.lat;
+      let longitude = response.coord.lon;
+      let uvQueryURL = "api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&APPID=" + owmAPI;
 
-  uvQueryURL = "https://cors-anywhere.herokuapp.com/" + uvQueryURL;
+      uvQueryURL = "https://cors-anywhere.herokuapp.com/" + uvQueryURL;
 
- fetch(uvQueryURL)
+      fetch(uvQueryURL)
         .then(handleErrors)
         .then((response) => {
-            return response.json();
+          return response.json();
         })
         .then((response) => {
-            let uvIndex = response.value;
-            $('#uvIndex').html(`UV Index: <span id="uvVal"> ${uvIndex}</span>`);
-            if (uvIndex>=0 && uvIndex<3){
-                $('#uvVal').attr("class", "uv-favorable");
-            } else if (uvIndex>=3 && uvIndex<8){
-                $('#uvVal').attr("class", "uv-moderate");
-            } else if (uvIndex>=8){
-                $('#uvVal').attr("class", "uv-severe");
-            }
+          let uvIndex = response.value;
+          $('#uvIndex').html(`UV Index: <span id="uvVal"> ${uvIndex}</span>`);
+          if (uvIndex >= 0 && uvIndex < 3) {
+            $('#uvVal').attr("class", "uv-favorable");
+          } else if (uvIndex >= 3 && uvIndex < 8) {
+            $('#uvVal').attr("class", "uv-moderate");
+          } else if (uvIndex >= 8) {
+            $('#uvVal').attr("class", "uv-severe");
+          }
         });
     })
 }
@@ -87,26 +86,26 @@ var getFiveDayForecast = (event) => {
   let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&APPID=" + owmAPI;
   // Fetch from API
   fetch(queryURL)
-      .then (handleErrors)
-      .then((response) => {
-          return response.json();
-      })
+    .then(handleErrors)
+    .then((response) => {
+      return response.json();
+    })
     .then((response) => {
 
       let fiveDayForecastHTML = `
         
         <div id="fiveDayForecastUl" class="d-inline-flex flex-wrap ">`;
-        // Loop over the 5 day forecast and build the template HTML using UTC offset and Open Weather Map icon
-        for (let i = 0; i < response.list.length; i++) {
-            let dayData = response.list[i];
-            let dayTimeUTC = dayData.dt;
-            let timeZoneOffset = response.city.timezone;
-            let timeZoneOffsetHours = timeZoneOffset / 60 / 60;
-            let thisMoment = moment.unix(dayTimeUTC).utc().utcOffset(timeZoneOffsetHours);
-            let iconURL = "https://openweathermap.org/img/w/" + dayData.weather[0].icon + ".png";
-            // Only displaying mid-day forecasts
-            if (thisMoment.format("HH:mm:ss") === "11:00:00" || thisMoment.format("HH:mm:ss") === "12:00:00" || thisMoment.format("HH:mm:ss") === "13:00:00") {
-                fiveDayForecastHTML += `
+      // Loop over the 5 day forecast and build the template HTML using UTC offset and Open Weather Map icon
+      for (let i = 0; i < response.list.length; i++) {
+        let dayData = response.list[i];
+        let dayTimeUTC = dayData.dt;
+        let timeZoneOffset = response.city.timezone;
+        let timeZoneOffsetHours = timeZoneOffset / 60 / 60;
+        let thisMoment = moment.unix(dayTimeUTC).utc().utcOffset(timeZoneOffsetHours);
+        let iconURL = "https://openweathermap.org/img/w/" + dayData.weather[0].icon + ".png";
+        // Only displaying mid-day forecasts
+        if (thisMoment.format("HH:mm:ss") === "11:00:00" || thisMoment.format("HH:mm:ss") === "12:00:00" || thisMoment.format("HH:mm:ss") === "13:00:00") {
+          fiveDayForecastHTML += `
                 <div class="weather-card card m-2 p0" style="background-color: #ffffffb7; font-family: Montserrat, sans-serif; color: #271334fb;">
         <ul class="list-unstyled p-3">
             <li>${thisMoment.format("MM/DD/YY")}</li>
@@ -116,12 +115,12 @@ var getFiveDayForecast = (event) => {
             <li>Humidity: ${dayData.main.humidity}%</li>
         </ul>
     </div>`;
-            }
         }
-        // Build the HTML template
-        fiveDayForecastHTML += `</div>`;
-        // Append the five-day forecast to the DOM
-        $('#five-day-forecast').html(fiveDayForecastHTML);
+      }
+      // Build the HTML template
+      fiveDayForecastHTML += `</div>`;
+      // Append the five-day forecast to the DOM
+      $('#five-day-forecast').html(fiveDayForecastHTML);
     })
 }
 
@@ -131,89 +130,89 @@ var saveCity = (newCity) => {
   let cityExists = false;
   // Check if City exists in local storage
   for (let i = 0; i < localStorage.length; i++) {
-      if (localStorage["cities" + i] === newCity) {
-          cityExists = true;
-          break;
-      }
+    if (localStorage["cities" + i] === newCity) {
+      cityExists = true;
+      break;
+    }
   }
   // Save to localStorage if city is new
   if (cityExists === false) {
-      localStorage.setItem('cities' + localStorage.length, newCity);
+    localStorage.setItem('cities' + localStorage.length, newCity);
   }
 }
 
 var renderCities = () => {
   $('#city-results').empty();
   // If localStorage is empty
-  if (localStorage.length===0){
-      if (lastCity){
-          $('#search-city').attr("value", lastCity);
-      } else {
-          $('#search-city').attr("value", "Austin");
-      }
-    } else {
-      // Build key of last city written to localStorage
-      let lastCityKey="cities"+(localStorage.length-1);
-      lastCity=localStorage.getItem(lastCityKey);
-      // Set search input to last city searched
+  if (localStorage.length === 0) {
+    if (lastCity) {
       $('#search-city').attr("value", lastCity);
-      // Append stored cities to page
-      for (let i = 0; i < localStorage.length; i++) {
-          let city = localStorage.getItem("cities" + i);
-          let cityEl;
-          // Set to lastCity if currentCity not set
-          if (currentCity===""){
-              currentCity=lastCity;
-
-            }
-            // Set button class to active for currentCity
-            if (city === currentCity) {
-                cityEl = `<button type="button" class="list-group-item list-group-item-action active">${city}</button></li>`;
-            } else {
-                cityEl = `<button type="button" class="list-group-item list-group-item-action">${city}</button></li>`;
-            } 
-            // Append city to page
-let $cityEl = $(cityEl);
-$cityEl.css({
-  "background-color": "#ffffff68", 
-  "color": "#2713347b",
-  "border-color": "#2713347b",
-  "font-family": "Montserrat, sans-serif",
-  "text-transform": "uppercase",
-  "font-weight": "bold",
-});
-$cityEl.hover(
-  function() { // function to run when mouse enters
-    $(this).css("background-color", "#ffffff");
-  }, 
-  function() { // function to run when mouse leaves
-    $(this).css("background-color", "#ffffff68");
-  }
-);
-
-$('#city-results').prepend($cityEl);
-        }
-        // Add a "clear" button to page if there is a cities list
-        if (localStorage.length>0){
-            $('#clear-storage').html($('<a id="clear-storage" href="#">clear</a>'));
-        } else {
-            $('#clear-storage').html('');
-        }
+    } else {
+      $('#search-city').attr("value", "Austin");
     }
-    
+  } else {
+    // Build key of last city written to localStorage
+    let lastCityKey = "cities" + (localStorage.length - 1);
+    lastCity = localStorage.getItem(lastCityKey);
+    // Set search input to last city searched
+    $('#search-city').attr("value", lastCity);
+    // Append stored cities to page
+    for (let i = 0; i < localStorage.length; i++) {
+      let city = localStorage.getItem("cities" + i);
+      let cityEl;
+      // Set to lastCity if currentCity not set
+      if (currentCity === "") {
+        currentCity = lastCity;
+
+      }
+      // Set button class to active for currentCity
+      if (city === currentCity) {
+        cityEl = `<button type="button" class="list-group-item list-group-item-action active">${city}</button></li>`;
+      } else {
+        cityEl = `<button type="button" class="list-group-item list-group-item-action">${city}</button></li>`;
+      }
+      // Append city to page
+      let $cityEl = $(cityEl);
+      $cityEl.css({
+        "background-color": "#ffffff68",
+        "color": "#2713347b",
+        "border-color": "#2713347b",
+        "font-family": "Montserrat, sans-serif",
+        "text-transform": "uppercase",
+        "font-weight": "bold",
+      });
+      $cityEl.hover(
+        function () { // function to run when mouse enters
+          $(this).css("background-color", "#ffffff");
+        },
+        function () { // function to run when mouse leaves
+          $(this).css("background-color", "#ffffff68");
+        }
+      );
+
+      $('#city-results').prepend($cityEl);
+    }
+    // Add a "clear" button to page if there is a cities list
+    if (localStorage.length > 0) {
+      $('#clear-storage').html($('<a id="clear-storage" href="#">clear</a>'));
+    } else {
+      $('#clear-storage').html('');
+    }
+  }
+
 }
 // New city search button event listener
 $('#search-button').on("click", (event) => {
   event.preventDefault();
   currentCity = $('#search-city').val();
   getCurrentConditions(event);
-  });
+});
 
-  // Old searched cities buttons event listener
+// Old searched cities buttons event listener
 $('#city-results').on("click", (event) => {
   event.preventDefault();
   $('#search-city').val(event.target.textContent);
-  currentCity=$('#search-city').val();
+  currentCity = $('#search-city').val();
   getCurrentConditions(event);
 });
 
@@ -229,4 +228,3 @@ renderCities();
 getCurrentConditions();
 
 
- 
